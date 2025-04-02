6ed6d50047de316cbf4e987d0a258ad67f5899b2
StanleyCUMT <1023039317@qq.com>
2025-04-02 15:14:06 +0800

    Poetry Update

--- scripts/grid_crm_launcher.py
+++ scripts/grid_crm_launcher.py
@@ -1,4 +1,5 @@
 import json
+import signal
 import argparse
 import c_two as cc
 
@@ -9,10 +10,10 @@ from crms.grid import CRM
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="CRM Launcher")
-    parser.add_argument('--temp', type=str, default=False, help="Use temporary memory")
+    parser.add_argument('--temp', type=str, default=False, help="Use temporary memory for grid")
     parser.add_argument('--schema_path', type=str, required=True, help="Path to the schema file")
     parser.add_argument('--tcp_address', type=str, required=True, help="TCP address for the server")
-    parser.add_argument('--grid_file_path', type=str, default='./grids.arrow', help="Path to the grid file")
+    parser.add_argument('--grid_file_path', type=str, required=True, help="Path to the grid file")
     args = parser.parse_args()
 
     # Grid parameters
@@ -23,10 +24,19 @@ if __name__ == '__main__':
     subdivide_rules = schema['subdivide_rules']
     ipc_address = 'ipc:///tmp/zmq_test'
     tcp_address = args.tcp_address
-    grid_file_path = None if args.temp == 'True' else args.grid_file_path
+    temp = args.temp == 'True'
+    grid_file_path = None if temp else args.grid_file_path
     
     # Init CRM
     crm = CRM(epsg, bounds, first_size, subdivide_rules, grid_file_path)
+
+    if sys.platform == 'win32':
+        try:
+            from ctypes import windll
+            kernel32 = windll.kernel32
+            kernel32.SetConsoleCtrlHandler(None, False)
+        except:
+            pass
     
     # Run CRM server
     server = cc.message.Server(tcp_address, crm)
