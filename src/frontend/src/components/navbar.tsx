9b62f879f450449c576f2de55ac4a5de519ba1d3
StanleyCUMT <1023039317@qq.com>
2025-04-16 16:22:34 +0800

    save

--- src/frontend/src/components/navbar.tsx
+++ src/frontend/src/components/navbar.tsx
@@ -1,20 +1,17 @@
 import * as React from 'react';
 import { cn } from '@/utils/utils';
-import { Search } from 'lucide-react';
+import {
+  Search,
+  Home,
+  Clipboard,
+  FilePlus2,
+  CircleHelp,
+  UsersRound,
+} from 'lucide-react';
 import { Switch } from '@/components/ui/switch';
 import { Label } from '@/components/ui/label';
-import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from '@/components/ui/dropdown-menu';
-import beststar from '../assets/beststar.jpg';
 import opengms from '../assets/opengms.png';
-import gridman from '../assets/gridman.png';
+import grid from '../assets/grid.png';
 import { useContext } from 'react';
 import { SidebarContext, LanguageContext } from '../App';
 
@@ -23,10 +20,13 @@ export interface NavbarProps extends React.HTMLAttributes<HTMLElement> {
   onNavItemClick?: (item: string) => void;
 }
 
-export function Navbar({ children, className, onNavItemClick, ...props }: NavbarProps) {
-  // 使用全局语言上下文
+export function Navbar({
+  children,
+  className,
+  onNavItemClick,
+  ...props
+}: NavbarProps) {
   const { language, setLanguage } = useContext(LanguageContext);
-  // 获取当前激活的侧边栏类型
   const { activeSidebar } = useContext(SidebarContext);
 
   const toggleLanguage = () => {
@@ -34,21 +34,23 @@ export function Navbar({ children, className, onNavItemClick, ...props }: Navbar
   };
 
   const navItems = [
-    { href: '/', labelZh: '首页', labelEn: 'Home' },
-    { href: '/schemas', labelZh: '模板', labelEn: 'Schema', type: 'schema' },
-    { href: '/new', labelZh: '新建', labelEn: 'New', type: 'operate' },
-    { href: '/help', labelZh: '帮助', labelEn: 'Help' },
-    { href: '/about', labelZh: '关于', labelEn: 'About' },
+    { href: '/', labelZh: '首页', labelEn: 'Home', icon: Home },
+    { href: '/schemas', labelZh: '模板', labelEn: 'Schema', type: 'schema', icon: Clipboard },
+    { href: '/new', labelZh: '新建', labelEn: 'New', type: 'operate', icon: FilePlus2 },
+    { href: '/help', labelZh: '帮助', labelEn: 'Help', icon: CircleHelp },
+    { href: '/about', labelZh: '关于', labelEn: 'About', icon: UsersRound },
   ];
 
-  const handleNavItemClick = (e: React.MouseEvent<HTMLAnchorElement>, item: string) => {
+  const handleNavItemClick = (
+    e: React.MouseEvent<HTMLAnchorElement>,
+    item: string
+  ) => {
     if (onNavItemClick) {
       e.preventDefault();
       onNavItemClick(item);
     }
   };
 
-  // 判断导航项是否处于激活状态
   const isActive = (type?: string) => {
     if (!type) return false;
     return type === activeSidebar;
@@ -57,35 +59,41 @@ export function Navbar({ children, className, onNavItemClick, ...props }: Navbar
   return (
     <nav
       className={cn(
-        'flex items-center justify-between p-4 border-b bg-black text-white h-20 z-50 relative',
+        'flex items-center justify-between p-4 border-b-2 bg-black text-white h-20 z-50 relative',
         className
       )}
       {...props}
     >
-      <div className="flex items-center gap-4 w-1/5">
-        <div className="flex items-center ml-5 ">
-          <img src={gridman} className="h-20 w-20" alt="Gridman"/>
+      <div className="flex items-center justify-center gap-4 w-1/5">
+        <div className="flex items-center">
+          <img src={grid} className="h-12 w-12 mr-4" alt="Gridman" />
           <a href="/" className="font-bold text-5xl text-white">
             Gridman
           </a>
         </div>
-        <div className="flex items-center ml-7 mr-2">
-          <a href="http://geomodeling.njnu.edu.cn/" target="_blank" rel="noopener noreferrer">
-            <img src={opengms} className="h-10 w-32" alt="OpenGMS" />
-          </a>
-        </div>
       </div>
-      <div className="flex items-center gap-6 mx-6 flex-grow">
+      <div className="flex items-center gap-10 mx-6 flex-grow -ml-1">
         {navItems.map((item, index) => (
           <a
             key={index}
             href={item.href}
             className={cn(
-              "transition-colors text-3xl font-bold",
-              isActive(item.type) ? "text-[#71F6FF]" : "text-gray-300 hover:text-white"
+              'transition-colors text-3xl font-bold flex items-center gap-2',
+              isActive(item.type)
+                ? 'text-[#71F6FF]'
+                : 'text-gray-300 hover:text-white'
             )}
-            onClick={(e) => handleNavItemClick(e, language === 'zh' ? item.labelZh : item.labelEn)}
+            onClick={(e) =>
+              handleNavItemClick(
+                e,
+                language === 'zh' ? item.labelZh : item.labelEn
+              )
+            }
           >
+            {React.createElement(item.icon, { 
+              className: 'w-10 h-10',
+              strokeWidth: 2
+            })}
             {language === 'zh' ? item.labelZh : item.labelEn}
           </a>
         ))}
@@ -102,7 +110,10 @@ export function Navbar({ children, className, onNavItemClick, ...props }: Navbar
       </div>
       <div className="flex items-center mr-20 ">
         <div className="flex items-center space-x-2">
-          <Label htmlFor="language-switch" className="text-sm text-gray-300">
+          <Label
+            htmlFor="language-switch"
+            className="text-md font-bold text-gray-300"
+          >
             EN
           </Label>
           <Switch
@@ -111,39 +122,22 @@ export function Navbar({ children, className, onNavItemClick, ...props }: Navbar
             onCheckedChange={() => toggleLanguage()}
             className="bg-gray-700 data-[state=checked]:bg-blue-500"
           />
-          <Label htmlFor="language-switch" className="text-sm text-gray-300">
-            中
+          <Label
+            htmlFor="language-switch"
+            className="text-md font-bold text-gray-300"
+          >
+            中文
           </Label>
         </div>
       </div>
-      <div className="flex items-center gap-2 ml-2">
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <Avatar className="cursor-pointer">
-              <AvatarImage src={beststar} />
-              <AvatarFallback>U</AvatarFallback>
-            </Avatar>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent align="end">
-            <DropdownMenuLabel>
-              {language === 'zh' ? '我的账户' : 'My Account'}
-            </DropdownMenuLabel>
-            <DropdownMenuSeparator />
-            <DropdownMenuItem>
-              {language === 'zh' ? '个人资料' : 'Profile'}
-            </DropdownMenuItem>
-            <DropdownMenuItem>
-              {language === 'zh' ? '设置' : 'Settings'}
-            </DropdownMenuItem>
-            <DropdownMenuSeparator />
-            <DropdownMenuItem>
-              {language === 'zh' ? '登录' : 'Login'}
-            </DropdownMenuItem>
-            <DropdownMenuItem>
-              {language === 'zh' ? '注册' : 'Register'}
-            </DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
+      <div className="flex items-center gap-2 ml-2 mr-5">
+        <a
+          href="http://geomodeling.njnu.edu.cn/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img src={opengms} className="h-15 w-48" alt="OpenGMS" />
+        </a>
       </div>
     </nav>
   );
