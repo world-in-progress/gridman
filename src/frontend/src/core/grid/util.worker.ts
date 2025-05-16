0a3ed26001ad704277b9b1f6b17c39f5a450c846
ld <723833066@qq.com>
2025-05-16 16:25:32 +0800

    remove grids

--- src/frontend/src/core/grid/util.worker.ts
+++ src/frontend/src/core/grid/util.worker.ts
@@ -30,7 +30,7 @@ export default class GridUtils {
 
   static async subdivideGrids(
     worker: WorkerSelf & Record<"gridManager", GridManager>,
-    gridInfo: { levels: Uint8Array; globalIds: Uint32Array },
+    gridInfo: { levels: Uint8Array; globalIds: Uint32Array }
   ) {
     const body = {
       levels: Array.from(gridInfo.levels),
@@ -42,10 +42,8 @@ export default class GridUtils {
       body
     );
     const { levels, globalIds } = multiGridInfo;
-    const [vertices, verticesLow] = worker.gridManager.createMultiRenderVertices(
-      levels,
-      globalIds
-    );
+    const [vertices, verticesLow] =
+      worker.gridManager.createMultiRenderVertices(levels, globalIds);
     const renderInfo: MultiGridRenderInfo = {
       levels,
       globalIds,
@@ -53,5 +51,30 @@ export default class GridUtils {
       verticesLow,
     };
     return renderInfo;
+  }
+
+  static async removeGrids(
+    gridInfo: { levels: Uint8Array; globalIds: Uint32Array }
+  ) {
+
+    const global_ids = Array.from(gridInfo.globalIds);
+    const body = {
+      levels: Array.from(gridInfo.levels),
+      global_ids,
+    };
+
+    const response = await fetch("/api/grid/operation/delete", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body),
+    });
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+
+    return global_ids;
   }
 }
