19c1f95d0749cd3ec6e342bc66acb729c478bc25
Stevnda <241302111@njnu.edu.cn>
2025-06-12 10:40:18 +0800

    Refactor API structure by removing obsolete operation files and updating feature service endpoints; adjust fetch methods in util.worker and feature APIs for consistency and improved error handling.

--- src/frontend/src/core/apis/feature.ts
+++ src/frontend/src/core/apis/feature.ts
@@ -6,12 +6,14 @@ import {
   FeatureGetJsonResponse,
 } from "../feature/types";
 
-const API_PREFIX = "/api/feature/operation";
+const API_PREFIX = "/server/api/feature";
 
 export const saveFeature: IAPI<FeatureSaveBody, FeatureSaveResponse> = {
   api: `${API_PREFIX}/save`,
   fetch: async (query: FeatureSaveBody): Promise<FeatureSaveResponse> => {
     try {
+      console.log("saveFeature");
+
       const response = await fetch(saveFeature.api, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
@@ -23,6 +25,7 @@ export const saveFeature: IAPI<FeatureSaveBody, FeatureSaveResponse> = {
       }
 
       const responseData: FeatureSaveResponse = await response.json();
+      console.log("responseData: ", responseData);
       return responseData;
     } catch (error) {
       throw new Error(`Failed to save feature: ${error}`);
