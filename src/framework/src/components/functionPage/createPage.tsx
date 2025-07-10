d2fdea56c680a29a17316a715df9d1ffb68457ad
YcSoku <dsssyc02@gmail.com>
2025-07-10 15:32:25 +0800

    Add WebGL triangle rendering with texture support
    - Implement vertex and fragment shaders for rendering a textured rectangle.
    - Create a Hello component that initializes a WebGL renderer and handles canvas resizing.
    - Develop HelloRenderer class to manage WebGL context, shaders, textures, and rendering logic.
    - Introduce glLib utility functions for shader creation, texture management, and error checking.
    - Load and bind a texture from an image for rendering.
    - Ensure proper cleanup of WebGL resources on component unmount.

--- src/framework/src/components/functionPage/createPage.tsx
+++ src/framework/src/components/functionPage/createPage.tsx
@@ -1,16 +1,11 @@
+import { cn } from '@/utils/utils'
 import MapContainer from '../mapContainer/mapContainer'
 import { CreatePageProps } from './types'
 
 export default function ResourcePage({ node }: CreatePageProps) {
-    if (!node) {
-        console.debug('Rendering MapContainer for null node')
-        return <MapContainer node={null} />
-    } else {
-        console.debug('Rendering page for valid node:', node.id)
-        return (
-            <div className='w-full h-full flex flex-row bg-[#1E1E1E]'>
-                {node.scenarioNode.renderPage(node)}
-            </div>
-        )
-    }
+    return (
+        <div className='w-full h-full flex flex-row bg-[#1E1E1E]'>
+            {node.scenarioNode.renderPage(node)}
+        </div>
+    )
 }
