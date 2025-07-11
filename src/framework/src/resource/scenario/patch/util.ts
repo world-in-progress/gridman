999132eb8d9e9f5ba043aa5c045090ca05e2241f
StanleyCUMT <1023039317@qq.com>
2025-07-11 16:24:31 +0800

    save

--- src/framework/src/resource/scenario/patch/util.ts
+++ src/framework/src/resource/scenario/patch/util.ts
@@ -4,8 +4,8 @@ import * as apis from '@/core/apis/apis'
 // Get Patch by schemaName and patchName
 export const getPatchInfo = async(node: SceneNode, isRemote: boolean) => {
     try {
-        const res = await apis.patch.getPatch.fetch({schemaName: node.parent!.parent!.name, patchName: node.name}, isRemote)
-        return res.patch_meta
+        const res = await apis.patch.getPatchMeta.fetch({schemaName: node.parent!.parent!.name, patchName: node.name}, isRemote)
+        return res
     } catch (error) {
         console.error('Get patch info failed: ', error)
         return null
