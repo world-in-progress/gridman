3aecefa56c9db47937a12e488fa0c84e2962ddd2
StanleyCUMT <1023039317@qq.com>
2025-07-23 16:45:12 +0800

    save

--- src/framework/src/resource/scenario/simulation/simulation.tsx
+++ src/framework/src/resource/scenario/simulation/simulation.tsx
@@ -1,10 +1,7 @@
 import DefaultPageContext from "@/core/context/default";
 import DefaultScenarioNode from "@/core/scenario/default";
 import { ISceneNode } from "@/core/scene/iscene";
-import SimulationPage from "./simulationPage"
-import { FilePlus, FilePlus2, FileType2, Info } from 'lucide-react'
-import { ContextMenuContent, ContextMenuItem } from '@/components/ui/context-menu'
-import { SceneNode, SceneTree } from "@/components/resourceScene/scene";
+import SimulationPage from "./simulationPage";
 
 export class SimulationPageContext extends DefaultPageContext {
     constructor() {
@@ -13,49 +10,17 @@ export class SimulationPageContext extends DefaultPageContext {
 }
 
 export enum SimulationMenuItem {
-    SIMULATION_INFORMATION = 'Simulation Information',
     SIMULATION = 'Simulation',
 }
 
 export default class SimulationScenarioNode extends DefaultScenarioNode {
-    static classKey: string = 'root.instances'
-    semanticPath: string = 'root.instances'
+    static classKey: string = 'root.icon.simulation'
+    semanticPath: string = 'root.icon.simulation'
     children: string[] = []
 
-    renderMenu(nodeSelf: ISceneNode, handleContextMenu: (node: ISceneNode, menuItem: any) => void): React.JSX.Element | null {
-        return (
-            <ContextMenuContent>
-                <ContextMenuItem className='cursor-pointer' onClick={() => handleContextMenu(nodeSelf, SimulationMenuItem.SIMULATION_INFORMATION)}>
-                    <Info className='w-4 h-4' />Simulation Information
-                </ContextMenuItem>
-                <ContextMenuItem className='cursor-pointer' onClick={() => handleContextMenu(nodeSelf, SimulationMenuItem.SIMULATION)}>
-                    <FilePlus2 className='w-4 h-4' />Simulation
-                </ContextMenuItem>
-            </ContextMenuContent>
-        )
-    }
-
-    handleMenuOpen(nodeSelf: ISceneNode, menuItem: any): void {
-        switch (menuItem) {
-            case SimulationMenuItem.SIMULATION:
-                (nodeSelf as SceneNode).pageId = 'default'
-                    ; (nodeSelf.tree as SceneTree).startEditingNode(nodeSelf as SceneNode)
-                break
-            case SimulationMenuItem.SIMULATION_INFORMATION:
-                (nodeSelf as SceneNode).pageId = 'information'
-                    ; (nodeSelf.tree as SceneTree).startEditingNode(nodeSelf as SceneNode)
-                break
-        }
-    }
-
     renderPage(nodeSelf: ISceneNode, menuItem: any): React.JSX.Element | null {
-        switch ((nodeSelf as SceneNode).pageId) {
-            case 'default':
-                return (<SimulationPage node={nodeSelf} />)
-            case 'information':
-                // return (<SimulationInformation node={nodeSelf} />)
-            default:
-                return (<SimulationPage node={nodeSelf} />)
-        }
+        return (
+            <SimulationPage />
+        )
     }
 }
