dd8f326f01814eb3d86fe9579d5624cb2291f040
ld <723833066@qq.com>
2025-07-26 11:33:53 +0800

    init terrain layer

--- src/framework/public/shaders/dems/show.glsl
+++ src/framework/public/shaders/dems/show.glsl
@@ -35,8 +35,32 @@ bool almostEqual(float a, float b) {
 }
 
 void main() {
+    vec4 color1 = texture(showTexture1, texcoords);
+    vec4 color2 = texture(showTexture2, texcoords);
 
-    fragColor = vec4(0.0, 0.0, 0.0, 0.5);
+    float alpha = mixAlpha;
+    if(almostEqual(color1.a, 0.0) || almostEqual(color2.a, 0.0)) {
+        alpha = 0.0;
+    }
+    vec4 color = mix(color1, color2, alpha);
+    // fragColor = vec4(color.rgb, color1.a);
+    // fragColor = vec4(color.rgb, 1.0);
+    // fragColor = vec4(color.rgb, 0.0);
+    // fragColor = vec4(color.rgb, color.a * 0.1);
+
+    if(color1.a == 1.0) {
+        fragColor = vec4(color1.rgb, 1.0);
+    } else {
+        fragColor = vec4(color.rgb, color.a * 0.1);
+    }
+
+    // vec3 rgb = mix(color1.rgb, color2.rgb, mixAlpha);
+    // fragColor = vec4(rgb, 0.1);
+    // fragColor = vec4(color.rgb, color.a * 0.1);
+
+    // fragColor = vec4(color.rgb, color.a * 0.7);
+    // fragColor = vec4(vec3(color1.r), alpha);
+    // fragColor = color1;
 }
 
 #endif
